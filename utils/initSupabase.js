38279dd39120266dd35ae733d4aa8196abc27204
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2020-11-12 00:08:18 +0800

    chore: better env var names.

--- utils/initSupabase.js
+++ utils/initSupabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export { supabase };
--- utils/initSupabaseAdmin.js
+++ utils/initSupabaseAdmin.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export { supabaseAdmin };
