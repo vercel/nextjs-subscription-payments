bd3dd980b00ef20bb35120969b3d7ac56f1a0337
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2020-11-11 20:17:45 +0800

    feat: order and render pricing.

--- components/Pricing.js
+++ components/Pricing.js
@@ -9,12 +9,42 @@ export default function Pricing() {
       // Load all products and prices.
       const { data: products, error } = await supabase
         .from('products')
-        .select('*, prices(*)');
+        .select('*, prices(*)')
+        .eq('active', true)
+        // .order('metadata:index')
+        .order('unit_amount', { foreignTable: 'prices' });
       if (error) alert(error.message);
       setProducts(products);
     }
     getProducts();
   }, []);
 
-  return <pre>{JSON.stringify(products, null, 2)}</pre>;
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          {product.image ? <img src={product.image} alt={product.name} /> : ''}
+          <h2>{product.name}</h2>
+          <p>{product.description}</p>
+          <form>
+            <label htmlFor="price">Choose pricing plan</label>
+            <select id="price" name="price">
+              {product.prices.map((price) => (
+                <option
+                  key={price.id}
+                  value={price.id}
+                >{`${new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: price.currency,
+                }).format((price.unit_amount / 100).toFixed(2))} per ${
+                  price.interval
+                }`}</option>
+              ))}
+            </select>
+            <button type="submit">Subscribe</button>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
 }
