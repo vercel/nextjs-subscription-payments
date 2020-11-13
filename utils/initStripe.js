0c80edfe938ed2a62c6b5cc01ac7016edf054a98
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2020-11-13 12:23:42 +0800

    feat: Add versioning, CHANGELOG, and set app info.

--- utils/initStripe.js
+++ utils/initStripe.js
@@ -3,6 +3,12 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: '2020-08-27',
+  // Register this as an official Stripe plugin.
+  // https://stripe.com/docs/building-plugins#setappinfo
+  appInfo: {
+    name: 'Vercel nextjs-saas-starter',
+    version: '0.1.0',
+  },
 });
 
 export { stripe };
