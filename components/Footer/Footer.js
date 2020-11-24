edc91df67c7611ccbdd752462cbef57d65089ca8
Lee Robinson <lrobinson2011@gmail.com>
2020-11-23 20:49:43 -0600

    Add Footer. (#12)

---
+++ components/Footer/Footer.js
@@ -0,0 +1,99 @@
+import Link from 'next/link';
+import Logo from '../Logo';
+import GitHub from '../GitHub';
+import s from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className="mx-auto max-w-8xl px-6">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-accents-2 py-12 text-primary bg-primary transition-colors duration-150">
+        <div className="col-span-1 lg:col-span-2">
+          <Link href="/">
+            <a className="flex flex-initial items-center font-bold md:mr-24">
+              <span className="rounded-full border border-gray-700 mr-2">
+                <Logo />
+              </span>
+              <span>ACME</span>
+            </a>
+          </Link>
+        </div>
+        <div className="col-span-1 lg:col-span-2">
+          <ul className="flex flex-initial flex-col md:flex-1">
+            <li className="py-3 md:py-0 md:pb-4">
+              <Link href="/">
+                <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
+                  Home
+                </a>
+              </Link>
+            </li>
+            <li className="py-3 md:py-0 md:pb-4">
+              <Link href="/">
+                <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
+                  Careers
+                </a>
+              </Link>
+            </li>
+            <li className="py-3 md:py-0 md:pb-4">
+              <Link href="/">
+                <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
+                  Blog
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-1 lg:col-span-2">
+          <ul className="flex flex-initial flex-col md:flex-1">
+            <li className="py-3 md:py-0 md:pb-4">
+              <Link href="/">
+                <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
+                  Terms of Use
+                </a>
+              </Link>
+            </li>
+            <li className="py-3 md:py-0 md:pb-4">
+              <Link href="/">
+                <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
+                  Shipping Returns
+                </a>
+              </Link>
+            </li>
+            <li className="py-3 md:py-0 md:pb-4">
+              <Link href="/">
+                <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
+                  Privacy Policy
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-1 lg:col-span-6 flex items-start lg:justify-end text-primary">
+          <div className="flex space-x-6 items-center h-10">
+            <a
+              aria-label="Github Repository"
+              href="https://github.com/vercel/nextjs-subscription-payments"
+              className={s.link}
+            >
+              <GitHub />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="py-12 flex flex-col md:flex-row justify-between items-center space-y-4">
+        <div>
+          <span>&copy; 2020 ACME, Inc. All rights reserved.</span>
+        </div>
+        <div className="flex items-center">
+          <span className="text-primary">Crafted by</span>
+          <a href="https://vercel.com" aria-label="Vercel.com Link">
+            <img
+              src="/vercel.svg"
+              alt="Vercel.com Logo"
+              className="inline-block h-6 ml-4 text-primary"
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../assets/main.css';
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 export default function MyApp({ Component, pageProps }) {
@@ -6,6 +7,7 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
