99d3db2c5c71293f717ea57750b0c4e73185cc8c
Lee Robinson <lrobinson2011@gmail.com>
2020-11-27 20:15:55 -0600

    Improve gray colors

--- pages/signin.js
+++ pages/signin.js
@@ -88,12 +88,12 @@ const SignIn = () => {
 
         <div className="flex items-center my-6">
           <div
-            className="border-t border-gray-300 flex-grow mr-3"
+            className="border-t border-accents-2 flex-grow mr-3"
             aria-hidden="true"
           ></div>
-          <div className="text-gray-600 italic">Or</div>
+          <div className="text-accents-4 italic">Or</div>
           <div
-            className="border-t border-gray-300 flex-grow ml-3"
+            className="border-t border-accents-2 flex-grow ml-3"
             aria-hidden="true"
           ></div>
         </div>
