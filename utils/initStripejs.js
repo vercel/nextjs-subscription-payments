40f20979be6e36c132c39208b84c49c565614154
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2020-12-11 11:34:11 +0800

    feat: support live mode Vercel integration.

--- utils/initStripejs.js
+++ utils/initStripejs.js
@@ -6,7 +6,10 @@ import { loadStripe } from '@stripe/stripe-js';
 let stripePromise;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+    stripePromise = loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_LIVE ??
+        process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+    );
   }
   return stripePromise;
 };
