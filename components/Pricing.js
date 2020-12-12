4e16b91a110a6731bc4324c25f0f7e50973c6bb5
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2020-12-12 14:15:33 +0800

    feat: Render notice and dashboard link when no products exist yet.

--- components/Pricing.js
+++ components/Pricing.js
@@ -34,6 +34,27 @@ export default function Pricing({ products }) {
     setLoading(false);
   };
 
+  if (!products.length)
+    return (
+      <section className="bg-black">
+        <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:flex-col sm:align-center"></div>
+          <p className="text-6xl font-extrabold text-white sm:text-center sm:text-6xl">
+            No subscription pricing plans found. Create them in your{' '}
+            <a
+              className="text-pink underline"
+              href="https://dashboard.stripe.com/products"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Stripe Dashboard
+            </a>
+            .
+          </p>
+        </div>
+      </section>
+    );
+
   return (
     <section className="bg-black">
       <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
