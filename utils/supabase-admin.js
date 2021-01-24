dd4eb356600b0b3bd2db9e0edf0fe26c68ab92a2
Lee Robinson <lrobinson2011@gmail.com>
2021-01-24 12:53:53 -0600

    Temporarily export

--- utils/supabase-admin.js
+++ utils/supabase-admin.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseAdmin = createClient(
+export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
