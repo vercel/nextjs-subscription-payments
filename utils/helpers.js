85c0e0c43bb21440ae9167cd01e3e0808d99cbb6
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2021-01-24 22:50:47 +0800

    fix: URL helper (#43)

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,10 @@
 const getURL = () => {
   const url =
-    process?.env?.URL ?? process?.env?.VERCEL_URL === ''
-      ? 'http://localhost:3000'
-      : process?.env?.VERCEL_URL ?? 'http://localhost:3000';
+    process?.env?.URL && process.env.URL !== ''
+      ? process.env.URL
+      : process?.env?.VERCEL_URL && process.env.VERCEL_URL !== ''
+      ? process.env.VERCEL_URL
+      : 'http://localhost:3000';
   return url.includes('http') ? url : `https://${url}`;
 };
 
