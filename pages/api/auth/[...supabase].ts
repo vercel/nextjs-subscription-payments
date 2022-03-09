ed9d8c3e89656c1ac152ecedb4ca0dc523071a1a
thorwebdev <thor@supabase.io>
2022-03-09 10:31:34 +0800

    chore: keep user logged in.

--- pages/api/auth/[...supabase].ts
+++ pages/api/auth/[...supabase].ts
@@ -1,3 +1,6 @@
 import { handleAuth } from '@supabase/supabase-auth-helpers/nextjs';
 
-export default handleAuth({ logout: { returnTo: '/signin' } });
+export default handleAuth({
+  logout: { returnTo: '/signin' },
+  cookieOptions: { lifetime: 1 * 365 * 24 * 60 * 60 } // Keep the user logged in for a year.
+});
