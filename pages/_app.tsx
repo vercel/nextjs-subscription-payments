06f0d9066fbe33d226010ce34ccc6eb0fae4afe4
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-27 11:58:28 -0400

    fix: height of 100% when in landscape mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import 'styles/main.css';
 import 'styles/chrome-bug.css';
-import { useEffect } from 'react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Layout from 'components/Layout';
 import { UserProvider } from '@supabase/supabase-auth-helpers/react';
