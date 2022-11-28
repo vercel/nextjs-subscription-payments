a40ee2bf148233c5ce3eb5f7bc3e7af2d3b8fd83
thorwebdev <thor@supabase.io>
2022-11-28 13:37:53 +0800

    chore: better url helper.

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,14 +1,15 @@
 import { Price } from 'types';
 
 export const getURL = () => {
-  const url =
-    process?.env?.URL && process.env.URL !== ''
-      ? process.env.URL
-      : process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-        process.env.NEXT_PUBLIC_VERCEL_URL !== ''
-      ? process.env.NEXT_PUBLIC_VERCEL_URL
-      : 'http://localhost:3000';
-  return url.includes('http') ? url : `https://${url}`;
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/';
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`;
+  // Make sure to including trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
 };
 
 export const postData = async ({
