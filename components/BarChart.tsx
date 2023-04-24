750c7209e718872157b031b8b4dbde050c361c41
Marcelo <marcelo@darocha.com>
2023-04-24 01:12:30 -0400

    fix string to number error

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -1,7 +1,7 @@
-import { useTheme } from "@mui/material";
-import { ResponsiveBar } from "@nivo/bar";
-import { tokens } from "../theme";
-import { mockBarData as data } from "../data/mockData";
+import { useTheme } from '@mui/material';
+import { ResponsiveBar } from '@nivo/bar';
+import { tokens } from '../theme';
+import { mockBarData as data } from '../data/mockData';
 
 const BarChart = ({ isDashboard = false }) => {
   const theme = useTheme();
@@ -15,60 +15,60 @@ const BarChart = ({ isDashboard = false }) => {
         axis: {
           domain: {
             line: {
-              stroke: colors.grey[100],
-            },
+              stroke: colors.grey[100]
+            }
           },
           legend: {
             text: {
-              fill: colors.grey[100],
-            },
+              fill: colors.grey[100]
+            }
           },
           ticks: {
             line: {
               stroke: colors.grey[100],
-              strokeWidth: 1,
+              strokeWidth: 1
             },
             text: {
-              fill: colors.grey[100],
-            },
-          },
+              fill: colors.grey[100]
+            }
+          }
         },
         legends: {
           text: {
-            fill: colors.grey[100],
-          },
-        },
+            fill: colors.grey[100]
+          }
+        }
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
+      keys={['hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut']}
       indexBy="country"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
-      valueScale={{ type: "linear" }}
-      indexScale={{ type: "band", round: true }}
-      colors={{ scheme: "nivo" }}
+      valueScale={{ type: 'linear' }}
+      indexScale={{ type: 'band', round: true }}
+      colors={{ scheme: 'nivo' }}
       defs={[
         {
-          id: "dots",
-          type: "patternDots",
-          background: "inherit",
-          color: "#38bcb2",
+          id: 'dots',
+          type: 'patternDots',
+          background: 'inherit',
+          color: '#38bcb2',
           size: 4,
           padding: 1,
-          stagger: true,
+          stagger: true
         },
         {
-          id: "lines",
-          type: "patternLines",
-          background: "inherit",
-          color: "#eed312",
+          id: 'lines',
+          type: 'patternLines',
+          background: 'inherit',
+          color: '#eed312',
           rotation: -45,
           lineWidth: 6,
-          spacing: 10,
-        },
+          spacing: 10
+        }
       ]}
       borderColor={{
-        from: "color",
-        modifiers: [["darker", "1.6"]],
+        from: 'color',
+        modifiers: [['darker', 1.6]]
       }}
       axisTop={null}
       axisRight={null}
@@ -76,52 +76,52 @@ const BarChart = ({ isDashboard = false }) => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "country", // changed
-        legendPosition: "middle",
-        legendOffset: 32,
+        legend: isDashboard ? undefined : 'country', // changed
+        legendPosition: 'middle',
+        legendOffset: 32
       }}
       axisLeft={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "food", // changed
-        legendPosition: "middle",
-        legendOffset: -40,
+        legend: isDashboard ? undefined : 'food', // changed
+        legendPosition: 'middle',
+        legendOffset: -40
       }}
       enableLabel={false}
       labelSkipWidth={12}
       labelSkipHeight={12}
       labelTextColor={{
-        from: "color",
-        modifiers: [["darker", 1.6]],
+        from: 'color',
+        modifiers: [['darker', 1.6]]
       }}
       legends={[
         {
-          dataFrom: "keys",
-          anchor: "bottom-right",
-          direction: "column",
+          dataFrom: 'keys',
+          anchor: 'bottom-right',
+          direction: 'column',
           justify: false,
           translateX: 120,
           translateY: 0,
           itemsSpacing: 2,
           itemWidth: 100,
           itemHeight: 20,
-          itemDirection: "left-to-right",
+          itemDirection: 'left-to-right',
           itemOpacity: 0.85,
           symbolSize: 20,
           effects: [
             {
-              on: "hover",
+              on: 'hover',
               style: {
-                itemOpacity: 1,
-              },
-            },
-          ],
-        },
+                itemOpacity: 1
+              }
+            }
+          ]
+        }
       ]}
       role="application"
       barAriaLabel={function (e) {
-        return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
+        return e.id + ': ' + e.formattedValue + ' in country: ' + e.indexValue;
       }}
     />
   );
