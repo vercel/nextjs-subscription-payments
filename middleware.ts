cac402c719c7c93aa97bcb2d2cc854f6e7d6ad0c
Matt <mr.matthewc.king@gmail.com>
2023-05-08 16:27:21 -0600

    work

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
-import type { Database } from '@/lib/database.types'
+import type { Database } from '@/types_db'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
