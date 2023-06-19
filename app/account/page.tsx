4799356ab4f4ef864e48ea4a2f314063ddfaabcd
Rounel <fabelseba@gmail.com>
2023-06-19 19:15:49 +0000

    edit file path again + add new RLS policy

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -60,7 +60,7 @@ export default async function Account() {
     const { data, error } = await supabase
       .storage
       .from('testfile')
-      .createSignedUrl('folder/CV FABEL SEBA.pdf', 5, {
+      .createSignedUrl('private/CV FABEL SEBA2.pdf', 5, {
         download: true,
       })
     if (data) {
