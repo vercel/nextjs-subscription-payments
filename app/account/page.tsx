298a322d5aaf2b26dc8c8fa8e4e06ee7ac8a14d5
Rounel <fabelseba@gmail.com>
2023-06-19 19:46:01 +0000

    ...

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -130,7 +130,7 @@ export default async function Account() {
                 variant="slim"
                 type="button"
                 disabled={subscription ? false : true}
-                onClick={() => downloadHb()}
+                onClick={downloadHb}
               >
                 {/* WARNING - In Next.js 13.4.x server actions are in alpha and should not be used in production code! */}
                 Download
