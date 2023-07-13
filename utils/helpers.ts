836de7754aacf738db7f2727de3f01eb1622096c
gooodsoil <131943124+gooodsoil@users.noreply.github.com>
2023-07-13 16:43:52 -0700

    Update helpers.ts for auth/callback
    Storing the user's session in a cookie does not work without this code.

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -8,9 +8,11 @@ export const getURL = () => {
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     'http://localhost:3000/';
   // Make sure to include `https://` when not localhost.
-  url = url.includes('http') ? url : `https://${url}`;
+  // Add `auth/callback` so you store the user's session in a cookie
+  url = url.includes('http')
+    ? `${url}auth/callback`
+    : `https://${url}auth/callback`;
   // Make sure to including trailing `/`.
-  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
   return url;
 };
 
