612ef696d3f5330a6e8307867d487260759e976d
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2023-07-16 07:50:35 +1000

    remove ts directive no longer required

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,6 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-black loading">
         <SupabaseProvider>
-          {/* @ts-expect-error */}
           <Navbar />
           <main
             id="skip"
