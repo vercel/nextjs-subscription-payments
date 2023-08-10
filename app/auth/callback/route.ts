94bb8999ff753cb912e3b6dd32a8040c6fcc741f
Philipp Maas <contact@philippmaas.com>
2023-08-10 14:43:55 +0200

    adjust db types pointer for api rout creat portal link

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,7 +2,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
-import type { Database } from '@/types_db'
+import type { Database } from '@/types/types_db'
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
