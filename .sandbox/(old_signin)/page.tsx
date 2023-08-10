982017008f8e95453b9c5c1f041896359eb57e22
Philipp Maas <contact@philippmaas.com>
2023-08-10 14:32:57 +0200

    move marketing and old sign in to sandbox folder

---
+++ .sandbox/(old_signin)/page.tsx
@@ -0,0 +1,24 @@
+import { getSession } from '@/app/supabase-server';
+import AuthUI from './AuthUI';
+
+import { redirect } from 'next/navigation';
+import Logo from '@/components/icons/Logo';
+
+export default async function SignIn() {
+  const session = await getSession();
+
+  if (session) {
+    return redirect('/account');
+  }
+
+  return (
+    <div className="flex justify-center height-screen-helper">
+      <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
+        <div className="flex justify-center pb-12 ">
+          <Logo width="64px" height="64px" />
+        </div>
+        <AuthUI />
+      </div>
+    </div>
+  );
+}
