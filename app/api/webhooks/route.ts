9e75417fe339a341d65cf6a1aaa850bc887bef1e
Matt <mr.matthewc.king@gmail.com>
2023-09-13 20:53:25 -0600

    fix webhooks route return type

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
   let event: Stripe.Event;
 
   try {
-    if (!sig || !webhookSecret) return;
+    if (!sig || !webhookSecret) return new Response("Webhook secret not found.", { status: 400 })
     event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
   } catch (err: any) {
     console.log(`❌ Error message: ${err.message}`);
