e6796509a82fa484ce5d5dcfd69896fb90aeaa19
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-11-14 15:16:00 -0500

    Update helpers.ts
    Re-added comments

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -4,8 +4,8 @@ type Price = Database['public']['Tables']['prices']['Row'];
 
 export const getURL = () => {
   let url =
-    (process.env.NEXT_PUBLIC_SITE_URL && process.env.NEXT_PUBLIC_SITE_URL.trim() !== '' ? process.env.NEXT_PUBLIC_SITE_URL :
-    process.env.NEXT_PUBLIC_VERCEL_URL && process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== '' ? process.env.NEXT_PUBLIC_VERCEL_URL :
+    (process.env.NEXT_PUBLIC_SITE_URL && process.env.NEXT_PUBLIC_SITE_URL.trim() !== '' ? process.env.NEXT_PUBLIC_SITE_URL : // Set this to your site URL in production env.
+    process.env.NEXT_PUBLIC_VERCEL_URL && process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== '' ? process.env.NEXT_PUBLIC_VERCEL_URL : // Automatically set by Vercel.
     'http://localhost:3000/').trim();
 
   // Make sure to include `https://` when not localhost.
