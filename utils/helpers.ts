fd3e0f26fecb9bc6ec0d271a2a5a5740047c0638
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-11-14 15:04:31 -0500

    Update helpers.ts
    Handle empty as well as missing env variables

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -4,12 +4,13 @@ type Price = Database['public']['Tables']['prices']['Row'];
 
 export const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/';
+    (process.env.NEXT_PUBLIC_SITE_URL && process.env.NEXT_PUBLIC_SITE_URL.trim() !== '' ? process.env.NEXT_PUBLIC_SITE_URL :
+    process.env.NEXT_PUBLIC_VERCEL_URL && process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== '' ? process.env.NEXT_PUBLIC_VERCEL_URL :
+    'http://localhost:3000/').trim();
+
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
-  // Make sure to including trailing `/`.
+  // Make sure to include trailing `/`.
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
   return url;
 };
