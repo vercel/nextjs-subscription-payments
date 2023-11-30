ac26a2e9fe175c8ca349443ca5c89f2c7c3a5d8d
Christopher Carroll Smith <chriscarrollsmith@gmail.com>
2023-11-29 23:03:12 -0500

    Allow stripe-js to use user's default API version

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -5,7 +5,8 @@ export const stripe = new Stripe(
   {
     // https://github.com/stripe/stripe-node#configuration
     // https://stripe.com/docs/api/versioning
-    apiVersion: '2022-11-15',
+    // @ts-ignore
+    apiVersion: null,
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
