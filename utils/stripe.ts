e50fc4384a6dded8c2733cb548fd4deeb7976cd7
Christopher Carroll Smith <chriscarrollsmith@gmail.com>
2023-11-29 22:12:34 -0500

    Rolled back stripe API version change

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -5,7 +5,7 @@ export const stripe = new Stripe(
   {
     // https://github.com/stripe/stripe-node#configuration
     // https://stripe.com/docs/api/versioning
-    apiVersion: '2023-10-16',
+    apiVersion: '2022-11-15',
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
