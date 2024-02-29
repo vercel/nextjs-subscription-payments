59f6f26caa151c352df041e8e86cbda72ee1b2ea
Leo <45669613+Hahlh@users.noreply.github.com>
2024-02-29 23:15:40 +0100

    Add missing hyphen in className of layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
         <Navbar />
         <main
           id="skip"
-          className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
+          className="min-h-[calc(100dvh-4rem)] md:min-h-[calc(100dvh-5rem)]"
         >
           {children}
         </main>
