feb44f4a72ccb76bcb47e56f3691d1ceb6edd93e
anthony <qiko0425@gmail.com>
2024-04-16 20:48:24 -0400

    Added/Fix Lifetime option

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -30,7 +30,7 @@ interface Props {
   subscription: SubscriptionWithProduct | null;
 }
 
-type BillingInterval = 'lifetime' | 'year' | 'month';
+type BillingInterval = null | 'year' | 'month';
 
 export default function Pricing({ user, products, subscription }: Props) {
   const intervals = Array.from(
@@ -141,6 +141,19 @@ export default function Pricing({ user, products, subscription }: Props) {
                   Yearly billing
                 </button>
               )}
+              {intervals.includes(null) && (
+                <button
+                  onClick={() => setBillingInterval(null)}
+                  type="button"
+                  className={`${
+                    billingInterval === null
+                      ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
+                      : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
+                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+                >
+                  Lifetime
+                </button>
+              )}
             </div>
           </div>
           <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 flex flex-wrap justify-center gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0">
@@ -179,7 +192,7 @@ export default function Pricing({ user, products, subscription }: Props) {
                         {priceString}
                       </span>
                       <span className="text-base font-medium text-zinc-100">
-                        /{billingInterval}
+                        {billingInterval !== null && `/${billingInterval}`}
                       </span>
                     </p>
                     <Button
